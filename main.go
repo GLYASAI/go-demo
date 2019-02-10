df19d2771c9a0cf19dbb9db7e526b5996b2121e7
GLYASAI <chaoschunqfeijg@163.com>
2019-02-10 22:45:02 +0800

    [ADD] add html and middleware

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"github.com/goodrain/go-demo/middleware"
 	"github.com/labstack/echo"
 )
 
 func main() {
 	e := echo.New()
+
+	middL := middleware.InitMiddleware()
+	e.Use(middL.CORS)
+
 	e.Static("/", "public")
+
 	e.Logger.Fatal(e.Start(":1323"))
 }
