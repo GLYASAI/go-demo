90db4b05db9d89f3bf33c6a6ce05c74af07f3e0c
GLYASAI <chaoschunqfeijg@163.com>
2019-02-11 18:21:56 +0800

    [ADD] add db info repository

--- main.go
+++ main.go
@@ -1,11 +1,32 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	dbinfo_repo "github.com/goodrain/go-demo/dbinfo/repository"
 	"github.com/goodrain/go-demo/middleware"
 	"github.com/labstack/echo"
+	"net/url"
+	"os"
 )
 
 func main() {
+	dbuser := os.Getenv("MYSQL_USER")
+	dbpw := os.Getenv("MYSQL_PASSWORD")
+	dbhost := os.Getenv("MYSQL_HOST")
+	dbport := os.Getenv("MYSQL_PORT")
+	dbname := os.Getenv("MYSQL_DATABASE")
+	conn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", dbuser, dbpw, dbhost, dbport, dbname)
+
+	val := url.Values{}
+	val.Add("parseTime", "1")
+	val.Add("loc", "Asia/Shanghai")
+	dsn := fmt.Sprintf("%s?%s", conn, val.Encode())
+	dbconn, _ := sql.Open(`mysql`, dsn)
+	defer dbconn.Close()
+
+	_ = dbinfo_repo.NewMysqlDBInfoRepository(dbconn)
+
 	e := echo.New()
 
 	middL := middleware.InitMiddleware()
